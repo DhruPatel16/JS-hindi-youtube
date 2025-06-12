c964ae129e15d4119c4dd67bb5aa8305c67ab0b9
DhruPatel16 <dhrupatel160603@gmail.com>
2025-06-12 13:34:34 +0000

    DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict" //treat all JS code as newer version 
+
+//alert(3 + 3) //we are using nodejs not browser
+
+console.log( 3 
+    +
+     3) // code readablility should be high
+
+console.log("Dhru")
+
+
+let name = "Dhru"
+let age = 22
+let isLoggedIn = true
+
+//number => 2 to the power 53
+//bigint
+//string => "" 
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+
+//object
+
+console.log(typeof "dhru");
+console.log(typeof undefined);// undefined
+console.log(typeof null);// object
