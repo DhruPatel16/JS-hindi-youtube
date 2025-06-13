39796537c34e579fba50cd3d77c1f79a7f4e9556
DhruPatel16 <dhrupatel160603@gmail.com>
2025-06-13 07:00:52 +0000

    Coversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "dhru"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let ValueInNumber = Number(score)
+console.log(typeof ValueInNumber);
+console.log(ValueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true =>1; false => 0
+
+let isLoggedIn = "dhru"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "dhru" => true
+
+let SomeNumber = 33
+
+let StringNumber = String(SomeNumber)
+console.log(StringNumber);
+console.log(typeof StringNumber);
+
